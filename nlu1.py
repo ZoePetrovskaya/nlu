dbed0eabb48a066570631c1f1b9bd9676fc37968
Zoe Petrovskaya <zoya0820@mail.ru>
2020-10-16 22:06:18 +0300

    доделанная nlu1

--- nlu1.py
+++ nlu1.py
@@ -1,39 +1,68 @@
+#import anaconda
 import random
+import nltk
 
 
 BOT_CONFIG = {
-"intent":{"hello":{"examples":["привет","как дела?"],"response":["привет","как дела?"] },
-"bye":{"examples":['пока',"покакашка"], "response":['пока',"покакашка"],},
-"failure_phrase":["перефразируйте","мне не понятно", "что-то я совсем непонятно"]}}
+    'intents': {'hello': {'examples': ['Привет', 'Здравствуйте', 'Добрый день',"привет","как дела?"],\
+                        'responses': ['Привет, человек', 'Здравствуйте!', 'Шалом, юзер']},\
+                'bye':   {'examples': ['Пока', 'Досвидания', 'Увидимся','пока',"покакашка"],\
+                        'responses': ['Прощай, человек. Приходи ещё.', 'Увидимся']}},\
+    'failure_phrases': ['Мне непонятно','Перефразируйте, пожалуйста','Не умею отвечать на такое',\
+           "перефразируйте","мне не понятно", "что-то я совсем непонятно"]\
+           }
 
 
 
 def get_failure_phrase():
-    failure_phrase =["перефразируйте","мне не понятно", "что-то я совсем непонятно"]
+    failure_phrase =BOT_CONFIG['failure_phrases']
     return random.choice(failure_phrase)
 
-#print(get_failure_phrase())
 
-def get_answer_by_intent(intent):
-    phrases = BOT_CONFIG["intents"][intent]['response']
-    return random.choice(phrases)
 
 def get_intent(question):
-    for x in BOT_CONFIG["intent"]:
-        print(x)
-    return "hello"
-intent ="пока"
-get_answer_by_intent(intent)
+    
+    for intent, intent_value in BOT_CONFIG["intents"].items():
+        #print(intent, intent_value['examples'])
+        for example in intent_value['examples']:
+            #print (example)
+            #if example != question:
+            d = nltk.edit_distance(example.lower(), question.lower())
+            #print(question)
+            diff = d/ len(example)
+            if diff <0.4:
+                return intent
+
+def get_generative_answer(question):
+    return
+
+def get_answer_by_intent(intent):
+    phrases = BOT_CONFIG["intents"][intent]['responses']
+    #return print(phrases[0])
+    return print(random.choice(phrases) )          
 
 def bot(question):
     #nlu
     intent = get_intent(question)
+   
     #получение ответа
     #заготовленные ответы
     if intent:
         return get_answer_by_intent(intent)
+    print(get_answer_by_intent(intent))
+    
     #генерация
     answer = get_generative_answer(question)
     if answer:
         return answer
+    #Заглушка
     return get_failure_phrase()
+
+
+
+
+
+question = "пивет"
+bot(question)
+print("161020")
+
